7f58b58791f2bd2d0a875e83d235f9d99ae0acbd
ranijhon <63198076+ranijhon@users.noreply.github.com>
2020-04-05 20:52:51 +0600

    Create hello-world.c

---
+++ hello-world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   printf("Hello World!");
+
+   printf("oh oh my program is running..\n");
+    
+   printf(6 + 6);
+  
+   printf(9 + 9);
+
+   return 0;
+
+}
